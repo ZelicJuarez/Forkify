52a91eb175ef188f5dbb934c60f3568b79209d81
Zelic Juarez <zelic.juamtz@gmail.com>
2019-06-17 17:42:52 -0500

    Building the Search Model

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,5 @@
-//All recipe requests should be made to the recipe details API URL.
-//https://www.food2fork.com/api/get
-import axios from "axios";
+import Search from "./models/Search";
 
-async function getResults(query) {
-  const key = "c95ec2732694b0f175af57e57b59ded9";
-  try {
-    const res = await axios(
-      `https://www.food2fork.com/api/search?key=${key}&q=${query}`
-    );
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch (error) {
-    alert(error);
-  }
-}
-getResults("tomato pasta");
+const search = new Search("pizza");
+console.log(search);
+search.getResults();
---
+++ src/js/models/Search.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  }
+
+  async getResults() {
+    const key = "c95ec2732694b0f175af57e57b59ded9";
+    try {
+      const res = await axios(
+        `https://www.food2fork.com/api/search?key=${key}&q=${this.query}`
+      );
+      this.results = res.data.recipes;
+      //console.log(this.results);
+    } catch (error) {
+      alert(error);
+    }
+  }
+}
