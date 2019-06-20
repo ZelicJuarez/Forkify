5fe4f80f5d2495a898d339678022063381e05289
Zelic Juarez <zelic.juamtz@gmail.com>
2019-06-20 12:15:22 -0500

    Building the Recipe View Part 1

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -55,6 +55,8 @@ export default class Recipe {
       "pound"
     ];
 
+    const units = [...unitsShort, 'kg', 'g'];
+
     const newIngredients = this.ingredients.map(el => {
       //1. Uniform units
       let ingredient = el.toLowerCase();
@@ -66,7 +68,7 @@ export default class Recipe {
 
       //3. Parse ingredients into count, unit and ingredient
       const arrIng = ingredient.split(" ");
-      const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2));
+      const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
 
       let objIng;
       if (unitIndex > -1) {
